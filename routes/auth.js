b595c55b55fdc7cef482e28fe3395d78b0a19dc3
Reetesh Kumar <rajreetesh7@gmail.com>
2023-04-16 02:38:06 +0530

    fixed the cookie

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/authController.js";
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/auth/register", registerUser);
 router.post("/auth/login", loginUser);
+router.get("/auth", logoutUser);
 
 export default router;
